fe5d9af6bd6fb4b59de8a7f2656b365defd36855
Monika Kashyap <monikakashyap14777@gmail.com>
2026-03-09 11:45:36 +0530

    09/03 last update time zone

--- src/main/java/com/smart_task/smart_task_manager_backend/SmartTaskManagerBackendApplication.java
+++ src/main/java/com/smart_task/smart_task_manager_backend/SmartTaskManagerBackendApplication.java
@@ -2,11 +2,14 @@ package com.smart_task.smart_task_manager_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication(scanBasePackages = {"com.smart_task.smart_task_manager_backend", "com.smart_task.smart_task_manager_backend.security"})
 public class SmartTaskManagerBackendApplication {
 
 	public static void main(String[] args) {
+        // Set server timezone
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(SmartTaskManagerBackendApplication.class, args);
 	}
 
